f4c6b2d9696a11631a5089fc69be6df14f26a9d2
Joel Rumerman <jrumerman@shift.com>
2016-03-15 11:03:01 -0400

    ptr

--- pointers.go
+++ pointers.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+func createSomething() map[string][]string {
+	x := make(map[string][]string)
+	fmt.Printf("memory address of x in createSomething: %p\n", x)
+	return x
+}
+
 func main() {
 	fmt.Printf("playing with pointers.\n")
 	i := 47
@@ -12,4 +18,7 @@ func main() {
 	fmt.Printf("memory address %p\n", p)                 // prints the memory address
 	fmt.Printf("value %d\n", *p)                         // dereferences the pointer to get the value.
 	fmt.Printf("memory address of the pointer %p\n", &p) //
+
+	y := createSomething()
+	fmt.Printf("memory address of y: %p\n", y)
 }
